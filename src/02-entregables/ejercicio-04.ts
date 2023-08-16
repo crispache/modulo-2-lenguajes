517b656df97b00e50462a195c31fb6d229817af3
crispache <cristinaglezpache06@gmail.com>
2023-08-16 20:13:20 +0100

    Añadir ejercicio 4 de entregables

--- src/02-entregables/ejercicio-04.ts
+++ src/02-entregables/ejercicio-04.ts
@@ -1,1 +1,25 @@
 console.log("************** DELIVERABLE 04 *********************");
+
+interface Book {
+    title: string;
+    isRead: boolean;
+}
+
+const books = [
+    { title: "Harry Potter y la piedra filosofal", isRead: true },
+    { title: "Canción de hielo y fuego", isRead: false },
+    { title: "Devastación", isRead: true },
+   /*  { title: "Canción de hielo y fuego", isRead: true }, */ 
+   // NOTA:  si estuviera duplicado y usara filter me devolvería todos los q cumple esa condición, >> find
+];
+
+
+const isBookRead = (books: Book[], titleToSearch: string): boolean => {
+    const book = books.find( (book) => book.title === titleToSearch)
+    return book?.isRead ? book.isRead : false;
+}
+
+
+console.log(isBookRead(books, "Devastación")); // true
+console.log(isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
--- transpiled/02-entregables/ejercicio-04.js
+++ transpiled/02-entregables/ejercicio-04.js
@@ -1,1 +1,15 @@
 console.log("************** DELIVERABLE 04 *********************");
+var books = [
+    { title: "Harry Potter y la piedra filosofal", isRead: true },
+    { title: "Canción de hielo y fuego", isRead: false },
+    { title: "Devastación", isRead: true },
+    /*  { title: "Canción de hielo y fuego", isRead: true }, */
+    // NOTA:  si estuviera duplicado y usara filter me devolvería todos los q cumple esa condición, >> find
+];
+var isBookRead = function (books, titleToSearch) {
+    var book = books.find(function (book) { return book.title === titleToSearch; });
+    return (book === null || book === void 0 ? void 0 : book.isRead) ? book.isRead : false;
+};
+console.log(isBookRead(books, "Devastación")); // true
+console.log(isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
