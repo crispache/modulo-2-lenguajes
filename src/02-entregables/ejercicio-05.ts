f8194270df146cf5e72dc09028945aa11a6c85a9
crispache <cristinaglezpache06@gmail.com>
2023-08-16 23:54:12 +0100

    Añadir ejercicio 5 de entregables

--- src/02-entregables/ejercicio-05.ts
+++ src/02-entregables/ejercicio-05.ts
@@ -1,1 +1,65 @@
 console.log("************** DELIVERABLE 05 *********************");
+
+
+class SlothMachine {
+   
+    private coins: number = 0;
+    private randomsBoolean: boolean[] = []
+
+    constructor() {}
+
+    public play(): void {
+        this.coins++;
+        this.checkIfHaveWonTheAward();
+    }
+
+    private checkIfHaveWonTheAward(): void {
+        this.generateRandomBoolean();
+        if(this.hasWon()) {
+            console.log(`Congratulations!!!. You won ${this.coins} coins!!`);  
+            this.resetCoins()
+        } else {
+            console.log("Good luck next time!!") 
+        }
+    }
+
+    private hasWon(): boolean {
+       if(this.randomsBoolean.length === 0) return false
+       return this.randomsBoolean.every( (element) => element === true);
+    }
+
+    private generateRandomBoolean(max = 3): void {
+        for (let index = 0; index < max; index++) {
+            this.randomsBoolean[index] = this.getRandomBoolean()
+        }
+    }
+
+    private getRandomBoolean(): boolean {
+        return Boolean(Math.floor(Math.random() * 2));
+    }
+
+    private resetCoins(): void {
+        this.coins = 0;
+    }
+   
+}
+
+const machine1 = new SlothMachine();
+console.log(machine1)
+machine1.play();
+machine1.play()
+machine1.play()
+machine1.play()
+machine1.play()
+machine1.play()
+
+
+const machine2 = new SlothMachine();
+console.log(machine2)
+machine2.play()
+machine2.play()
+machine2.play()
+machine2.play()
+machine2.play()
+machine2.play()
+
--- transpiled/02-entregables/ejercicio-05.js
+++ transpiled/02-entregables/ejercicio-05.js
@@ -1,1 +1,55 @@
 console.log("************** DELIVERABLE 05 *********************");
+var SlothMachine = /** @class */ (function () {
+    function SlothMachine() {
+        this.coins = 0;
+        this.randomsBoolean = [];
+    }
+    SlothMachine.prototype.play = function () {
+        this.coins++;
+        this.checkIfHaveWonTheAward();
+    };
+    SlothMachine.prototype.checkIfHaveWonTheAward = function () {
+        this.generateRandomBoolean();
+        if (this.hasWon()) {
+            console.log("Congratulations!!!. You won ".concat(this.coins, " coins!!"));
+            this.resetCoins();
+        }
+        else {
+            console.log("Good luck next time!!");
+        }
+    };
+    SlothMachine.prototype.hasWon = function () {
+        if (this.randomsBoolean.length === 0)
+            return false;
+        return this.randomsBoolean.every(function (element) { return element === true; });
+    };
+    SlothMachine.prototype.generateRandomBoolean = function (max) {
+        if (max === void 0) { max = 3; }
+        for (var index = 0; index < max; index++) {
+            this.randomsBoolean[index] = this.getRandomBoolean();
+        }
+    };
+    SlothMachine.prototype.getRandomBoolean = function () {
+        return Boolean(Math.floor(Math.random() * 2));
+    };
+    SlothMachine.prototype.resetCoins = function () {
+        this.coins = 0;
+    };
+    return SlothMachine;
+}());
+var machine1 = new SlothMachine();
+console.log(machine1);
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+machine1.play();
+var machine2 = new SlothMachine();
+console.log(machine2);
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
+machine2.play();
