3887979b7aa40bf6d79eec55b6c728078d62432d
crispache <cristinaglezpache06@gmail.com>
2023-08-29 21:18:20 +0100

    Revisar y modificar el entregable 3

--- src/02-entregables/ejercicio-03.ts
+++ src/02-entregables/ejercicio-03.ts
@@ -1,8 +1,16 @@
 console.log("************** DELIVERABLE 03 *********************");
 console.log("");
 
-console.log("************** Example function clone *********************");
+const user_1: Person = { name: 'Manuel', age: 30 };
+const user_2: Person = { name: "Maria", surname: "Ibañez", country: "SPA" };
+const user_3: Person = { name: "Luisa", age: 31, married: true };
+console.log("**** Object 1 ****" , user_1)
+console.log("**** Object 2 ****", user_2)
+console.log("**** Object 3 ****", user_3)
+console.log("");
 
+
+console.log("************** Example function clone *********************");
 
 interface Person {
     name: string;
@@ -13,26 +21,19 @@ interface Person {
 }
 
 
-//* Nota: copia sencilla(a primer nivel) no es una copia profunda.
+// Nota: copia sencilla (a primer nivel) no es una copia profunda.
 const clone = <T extends object>(source: T): T => {    
     return {...source};
 } 
 
-const merge = <T extends object>(source: T, target: T) => {    // TIP: Puedes usar la función "clone" del apartado A.
+const objectCopy = clone<Person>(user_1)
+console.log('*** Object 1 (Clone) -> ', objectCopy)
+
+const merge = <T extends object>(source: T, target: T): T => {
     const copy_target = clone<T>(target);
     const copy_source = clone<T>(source);
     return {...copy_target, ...copy_source}
 } 
 
-
-const user_1: Person = { name: 'Manuel', age: 30 };
-const user_2: Person = { name: "Maria", surname: "Ibañez", country: "SPA" };
-const user_3: Person = { name: "Luisa", age: 31, married: true };
-
-const objectCopy = clone<Person>(user_1)
-console.log('clone -> ', objectCopy)
-
-/*-----------------------------------------------------------------*/
-
 const mergeArrays = merge<Person>(user_2, user_3);
-console.log('Merge -> ', mergeArrays)
+console.log('*** Object 2 - Object 3 (Merge) -> ', mergeArrays)
--- transpiled/02-entregables/ejercicio-03.js
+++ transpiled/02-entregables/ejercicio-03.js
@@ -11,21 +11,24 @@ var __assign = (this && this.__assign) || function () {
 };
 console.log("************** DELIVERABLE 03 *********************");
 console.log("");
+var user_1 = { name: 'Manuel', age: 30 };
+var user_2 = { name: "Maria", surname: "Ibañez", country: "SPA" };
+var user_3 = { name: "Luisa", age: 31, married: true };
+console.log("**** Object 1 ****", user_1);
+console.log("**** Object 2 ****", user_2);
+console.log("**** Object 3 ****", user_3);
+console.log("");
 console.log("************** Example function clone *********************");
-//* Nota: copia sencilla(a primer nivel) no es una copia profunda.
+// Nota: copia sencilla (a primer nivel) no es una copia profunda.
 var clone = function (source) {
     return __assign({}, source);
 };
+var objectCopy = clone(user_1);
+console.log('*** Object 1 (Clone) -> ', objectCopy);
 var merge = function (source, target) {
     var copy_target = clone(target);
     var copy_source = clone(source);
     return __assign(__assign({}, copy_target), copy_source);
 };
-var user_1 = { name: 'Manuel', age: 30 };
-var user_2 = { name: "Maria", surname: "Ibañez", country: "SPA" };
-var user_3 = { name: "Luisa", age: 31, married: true };
-var objectCopy = clone(user_1);
-console.log('clone -> ', objectCopy);
-/*-----------------------------------------------------------------*/
 var mergeArrays = merge(user_2, user_3);
-console.log('Merge -> ', mergeArrays);
+console.log('*** Object 2 - Object 3 (Merge) -> ', mergeArrays);
